4bfc339a7e219848004f206fcef80565e777ccef
Ivanka58 <kalamaevi0@gmail.com>
2026-02-01 16:29:05 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -79,5 +79,5 @@ async def root():
     return {"message": "Telegram Currency Converter Bot is running!"}
 
 if __name__ == "__main__":
-    PORT = int(os.environ.get("PORT", 3000))
+    PORT = int(os.environ.get("PORT", 0))  # Автоматический выбор порта
     uvicorn.run(app, host="0.0.0.0", port=PORT)
